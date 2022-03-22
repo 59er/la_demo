b57068ad739d7c3c1ba41a4049a355ba40f0ded5
59er <69283605+59er@users.noreply.github.com>
2022-03-22 11:40:17 +0900

    Update eng_main_la_demo_app.py

--- eng_main_la_demo_app.py
+++ eng_main_la_demo_app.py
@@ -17,7 +17,7 @@ from eng_course_recommend_app import run_course_recommend_app
 from eng_sampling_adequacy_app import run_sampling_adequacy_app
 
 def main():
-    st.title("教育データ分析WEB\n (β version)")
+    st.title("教育データ分析WEB(β version)")
 
     menu = ["HOME",'Overview of learning situation','Detailed analysis',
             'Correlation analysis',
